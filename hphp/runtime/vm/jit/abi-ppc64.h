89d2dd3aea867602da316b1026d3fc346b6b433e
rcardoso <rcardoso@dhcp-9-18-235-243.br.ibm.com>
2015-07-22 15:20:14 -0300

    Added draft and some pseudo-code for ppc64 abi. Need to validade info.

--- hphp/runtime/vm/jit/abi-ppc64.h
+++ hphp/runtime/vm/jit/abi-ppc64.h
@@ -22,14 +22,36 @@
 
 namespace HPHP { namespace jit { namespace ppc64 {
 
+/*
+  TODO(IBM):This is a draft for PPC64 abi, must validate all these informations
 
-//TODO(IBM): Implement PPC64 Abi
- 
+*/
+// constexpr RegisterPhys rVmFp(reg::gpr1) // Frame Pointer (PPC has no frame pointer?)
+// constexpr RegisterPhys rVmSp(reg::gpr1) //Stack Pointer
+// constexpr RegisterPhys rAsm(reg::grp0) //Scratch Register 
+// constexpr PhysReg rVmToC(reg::gpr2) //Base of Stack 
+// constexpr RegisterPhys rLinkReg(reg::rLK) //Link register
+// constexpr RegisterPhys rReturnReg(reg::gpr3) //Return Register
+
+//kGPReserved =  rLinkReg | rVmSp //Maybe CR register here ?
+
+//const RegSet kGPUnreserved = kGPCallerSaved | kGPCalleeSaved; 
+
+//This is calling conventions
+//const RegSet kGPCallerSaved = reg::gpr0 | reg::gpr3 | reg::gpr4 | reg::gpr5 | reg::gpr6 | reg::gpr7 | reg::gpr8 | reg::gpr9 | 
+// reg::gpr10 | reg::gpr11 | reg::gpr12 | reg::gpr13 | + CTR, CR0, CR1, CR5-7  
+
+// const RegSet kGPCalleSaved = Callee-save: r14-r31, CR2-4 
+
+//const RegSet kSF = RegSet(RegSF{0});
+
+ /*
+   TODO(IBM) Need to check ABI for Vector Instructions.
+ */
+
 // UNUSED const Abi abi {
 //   kGPUnreserved,
 //   kGPReserved,
-//   kSIMDUnreserved,
-//   kSIMDReserved,
 //   kCalleeSaved,
 //   kSF 
 // };
