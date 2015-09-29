861de7296a8dcaabc2dc04f143789efca8805020
Breno Leitao <brenohl@br.ibm.com>
2015-09-29 11:04:04 -0400

    Fix previous commit related using the proper ppc asm
    The previous commit created a new asm file, but there is one already at
    ppc64-asm directory.

--- hphp/runtime/vm/jit/code-gen-tls-ppc64.h
+++ hphp/runtime/vm/jit/code-gen-tls-ppc64.h
@@ -21,7 +21,7 @@
 #include "hphp/runtime/vm/jit/vasm-instr.h"
 #include "hphp/runtime/vm/jit/vasm-reg.h"
 
-#include "hphp/util/asm-ppc64.h"
+#include "hphp/ppc64-asm/asm-ppc64.h"
 #include "hphp/util/thread-local.h"
 
 namespace HPHP { namespace jit { namespace ppc64 { namespace detail {
