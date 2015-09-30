c0d7d63bd80e4a710a1f071961a0b526ceddcf67
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2015-09-30 15:07:25 -0300

    Fix vasm xorl implementation, for now
    mwang said FB is providing a solution to this problem internally. There
    was a misscomunication and I fix it this morning on the
    'for-facebook-vasm-zero' branch. Check commit Id
    0a5ba855c378016e5e091282fa0df09c08d84a4e . But that is not going
    upstream for now :(

--- hphp/runtime/vm/jit/vasm-ppc64.cpp
+++ hphp/runtime/vm/jit/vasm-ppc64.cpp
@@ -616,6 +616,9 @@ struct Vgen {
   void emit(const ucomisd& i) { not_implemented(); }
   void emit(const ud2& i) { a->trap(); }
   void emit(unpcklpd i) { not_implemented(); }
+  void emit(xorb i) { a->xor_(Reg64(i.d), Reg64(i.s0), Reg64(i.s1), false); }
+  void emit(xorbi i) { a->xori(Reg64(i.d), Reg64(i.s1), i.s0); }
+  void emit(xorl i) { a->xor_(Reg64(i.d), Reg64(i.s0), Reg64(i.s1), false); }
   void emit(xorq i) { a->xor_(i.d, i.s0, i.s1, false); }
   void emit(xorqi i) { a->xori(i.d, i.s1, i.s0); }
 
