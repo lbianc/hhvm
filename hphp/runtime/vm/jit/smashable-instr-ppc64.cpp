70fc1656e1e73e1df4b1b940db6b82889e60fa4a
Rogerio Alves <rogealve@br.ibm.com>
2015-10-13 06:56:28 -0500

    Fix trailing white space.

--- hphp/runtime/vm/jit/smashable-instr-ppc64.cpp
+++ hphp/runtime/vm/jit/smashable-instr-ppc64.cpp
@@ -67,7 +67,7 @@ TCA emitSmashableCmpq(CodeBlock& cb, int32_t imm, PhysReg r, int8_t disp) {
   a.ld(rvasmtmp() ,r[disp]);
   a.cmpdi(rvasmtmp(), imm);
 
-  return start; 
+  return start;
 }
 
 TCA emitSmashableCall(CodeBlock& cb, TCA target) {
