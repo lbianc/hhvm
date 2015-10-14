675aa2bf3b33bcc9cf438239f0411b3f9114f746
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2015-10-14 15:37:05 -0300

    Fix compilation: can't clobber register r31
    Forgot this local change on 02247501a599171f887a1ca8e3bb48728015a997

--- hphp/runtime/vm/jit/back-end-x64.cpp
+++ hphp/runtime/vm/jit/back-end-x64.cpp
@@ -81,7 +81,7 @@ struct BackEnd final : jit::BackEnd {
       asm volatile("" : : : "r14", "r15", "r16", "r17", "r18",      \
                             "r19", "r20", "r21", "r22", "r23",      \
                             "r24", "r25", "r26", "r27", "r28",      \
-                            "r29", "r30", "r31");
+                            "r29", "r30");
 #else
   #define CALLEE_SAVED_BARRIER()                                    \
       asm volatile("" : : : "rbx", "r12", "r13", "r14", "r15");
