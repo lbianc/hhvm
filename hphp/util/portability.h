4a90b89303c568d7dd303258ec01b28588c7202a
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2015-11-25 09:04:14 -0200

    Improve non-volatile register saving with floats and vectors
    The CALLEE_SAVED_BARRIER macro already points out to gcc that those
    registers need to be saved before VM is entered, therefore this callee
    saved registers (non-volatile) doesn't need to be saved again in
    enterTCHelper and restored in enterTCExit.
    
    For some reason the $f registers couldn't be saved that way, so I'm
    using the traditional stack approach for them

--- hphp/util/portability.h
+++ hphp/util/portability.h
@@ -174,7 +174,8 @@
   #define CALLEE_SAVED_BARRIER()\
     asm volatile("" : : : "r2", "r14", "r15", "r16", "r17", "r18", "r19",\
         "r20", "r21", "r22", "r23", "r24", "r25", "r26", "r27", "r28",   \
-        "r29", "r30");
+        "r29", "r30", "cr2", "cr3", "cr4", "v20", "v21", "v22", "v23",   \
+        "v24", "v25", "v26", "v27", "v28", "v29", "v30", "v31");
 #elif defined (__AARCH64EL__)
   #define CALLEE_SAVED_BARRIER()\
     asm volatile("" : : : "x19", "x20", "x21", "x22", "x23", "x24", "x25",\
