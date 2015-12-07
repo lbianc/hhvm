b1eec6a298f1dcd6c8bb84576a2d3f94bc04f3f6
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2015-12-07 18:43:03 -0200

    Remove unused rbackchain()
    The fix provided by this commit's parent made it obsolete.

--- hphp/runtime/vm/jit/abi-ppc64.h
+++ hphp/runtime/vm/jit/abi-ppc64.h
@@ -50,7 +50,6 @@ constexpr PhysReg rfuncentry() { return ppc64_asm::reg::r12; }
 // CR depending the instruction result and instructions like "addi" (using
 // immediate) does not set the CR.
 constexpr PhysReg rone()       { return ppc64_asm::reg::r27; }
-constexpr PhysReg rbackchain() { return ppc64_asm::reg::r26; }
 
 namespace detail {
   const RegSet kVMRegs      = rvmfp() | rvmtl() | rvmsp();
