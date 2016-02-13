aa5413154870e2545478c24b914c2316cf9839d4
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2016-02-13 15:14:28 -0200

    Throw not-implemented on our decoded-instr
    It makes easier to notice the bug if it hangs there. Don't let a default
    return be there always (I left it there for compiling purposes).

--- hphp/ppc64-asm/decoded-instr-ppc64.cpp
+++ hphp/ppc64-asm/decoded-instr-ppc64.cpp
@@ -40,36 +40,13 @@ void DecodedInstruction::decode(uint8_t* ip) {
 }
 
 static int64_t readValue(uint8_t* ip, int size) {
-#if 0 // TODO(gut)
-  int64_t value = 0;
-  value = (signed char)ip[--size];
-  while (size--) {
-    value <<= 8;
-    value += ip[size];
-  }
-  return value;
-#else
+  not_implemented();
   return 0;
-#endif
 }
 
 static bool writeValue(uint8_t* ip, int size, int64_t v) {
-#if 0 // TODO(gut)
-  auto value = uint64_t(v);
-  if (size * CHAR_BIT < 64) {
-    auto topBit = uint64_t(1) << (size * CHAR_BIT - 1);
-    if (value + topBit >= topBit * 2) return false;
-  }
-
-  while (size--) {
-    *ip++ = (uint8_t)value;
-    value >>= CHAR_BIT;
-  }
+  not_implemented();
   return true;
-
-#else
-  return true;
-#endif
 }
 
 std::string DecodedInstruction::toString() {
@@ -77,91 +54,33 @@ std::string DecodedInstruction::toString() {
   auto str = folly::format("{:08x} {:02x}",
                            (uint64_t)m_ip,
                            m_opcode).str();
-#if 0 // TODO(gut)
-  if (m_flags.hasModRm) {
-    auto modRm = getModRm();
-    str += folly::format(" ModRM({:02b} {} {})",
-                         modRm >> 6,
-                         (modRm >> 3) & 7,
-                         modRm & 7).str();
-    if (m_flags.hasSib) {
-      auto sib = m_ip[m_size - m_immSz - m_offSz - 1];
-      str += folly::format(" SIB({:02b} {} {})",
-                           sib >> 6,
-                           (sib >> 3) & 7,
-                           sib & 7).str();
-    }
-  }
-
-  auto ip = m_ip + m_size - m_immSz - m_offSz;
-  if (m_offSz) {
-    int64_t value = readValue(ip, m_offSz);
-    ip += m_offSz;
-    str += folly::format(" {}{:+x}",
-                         m_flags.picOff ? "rip" : "",
-                         value).str();
-    if (m_flags.picOff) {
-      str += folly::format("({:08x})", uintptr_t(m_ip + m_size + value)).str();
-    }
-  }
-
-  if (m_immSz) {
-    int64_t value = readValue(ip, m_immSz);
-    ip += m_immSz;
-    str += folly::format(" #{}", value).str();
-  }
-#endif
+  not_implemented();
   return str;
 }
 
 int32_t DecodedInstruction::offset() const {
-#if 0 // TODO(gut)
-  assert(hasOffset());
-  auto const addr = m_ip + m_size;
-  return safe_cast<int32_t>(readValue(addr - m_offSz, m_offSz));
-#else
+  not_implemented();
   return 0;
-#endif
 }
 
 uint8_t* DecodedInstruction::picAddress() const {
-#if 0 // TODO(gut)
-  assert(hasPicOffset());
-  uint8_t* addr = m_ip + m_size;
-  return addr + readValue(addr - m_immSz - m_offSz, m_offSz);
-#else
+  not_implemented();
   return nullptr;
-#endif
 }
 
 bool DecodedInstruction::setPicAddress(uint8_t* target) {
-#if 0 // TODO(gut)
-  assert(hasPicOffset());
-  uint8_t* addr = m_ip + m_size;
-  ptrdiff_t diff = target - addr;
-
-  return writeValue(addr - m_offSz - m_immSz, m_offSz, diff);
-#else
+  not_implemented();
   return true;
-#endif
 }
 
 int64_t DecodedInstruction::immediate() const {
-#if 0 // TODO(gut)
-  assert(hasImmediate());
-  return readValue(m_ip + m_size - m_immSz, m_immSz);
-#else
+  not_implemented();
   return 0;
-#endif
 }
 
 bool DecodedInstruction::setImmediate(int64_t value) {
-#if 0 // TODO(gut)
-  assert(hasImmediate());
-  return writeValue(m_ip + m_size - m_immSz, m_immSz, value);
-#else
+  not_implemented();
   return true;
-#endif
 }
 
 bool DecodedInstruction::isNop() const {
@@ -186,12 +105,8 @@ bool DecodedInstruction::isJmp() const {
 }
 
 bool DecodedInstruction::isLea() const {
-#if 0 // TODO(gut)
-  if (m_map_select != 0) return false;
-  return m_opcode == 0x8d;
-#else
+  not_implemented();
   return false;
-#endif
 }
 
 bool DecodedInstruction::isClearSignBit() const {
@@ -199,81 +114,21 @@ bool DecodedInstruction::isClearSignBit() const {
 }
 
 HPHP::jit::ConditionCode DecodedInstruction::jccCondCode() const {
-#if 0 // TODO(gut)
-  if (m_map_select == 0) {
-    assert((m_opcode & 0xf0) == 0x70); // 8-bit jcc
-  } else {
-    assert(m_map_select == 1);
-    assert((m_opcode & 0xf0) == 0x80); // 32-bit jcc
-  }
-  return static_cast<ConditionCode>(m_opcode & 0x0f);
-#else
+  not_implemented();
   return HPHP::jit::CC_None;
-#endif
 }
 
 bool DecodedInstruction::shrinkBranch() {
-#if 0 // TODO(gut)
-  assert(isBranch());
-  if (m_offSz != sz::dword) return false;
-  auto addr = m_ip + m_size - m_offSz;
-  auto delta = readValue(addr, m_offSz);
-  if (m_map_select == 1) {
-    if (m_flags.vex) return false;
-    assert((m_opcode & 0xf0) == 0x80); // must be a 32-bit conditional branch
-    /*
-      The pc-relative offset is from the end of the instruction, and the
-      instruction is shrinking by 4 bytes (opcode goes from 2 bytes to 1,
-      and offset goes from 4 to 1), so we need to adjust delta by 4.
-    */
-    delta += 4;
-    if (-128 > delta || delta > 127) return false;
-    addr[-2] = 0x70 | (m_opcode & 0x0f); // make it an 8 bit conditional branch
-    addr[-1] = delta;
-  } else {
-    assert(m_opcode == 0xe9); // must be a 32-bit unconditional branch
-    /*
-      As above, but opcode was already 1 byte, so the reduction is only 3
-      bytes this time.
-    */
-    delta += 3;
-    if (-128 > delta || delta > 127) return false;
-    addr[-1] = 0xeb;
-    addr[0] = delta;
-  }
-  decode(m_ip);
-  assert(isBranch() && m_offSz == 1);
+  not_implemented();
   return true;
-#else
-  return true;
-#endif
 }
 
 void DecodedInstruction::widenBranch() {
-#if 0 // TODO(gut)
-  assert(m_offSz == 1 && isBranch());
-  auto addr = m_ip + m_size - m_offSz;
-  auto delta = readValue(addr, 1);
-  if (m_opcode == 0xeb) {
-    addr[-1] = 0xe9;
-    writeValue(addr, 4, delta + 3);
-  } else {
-    addr[-1] = 0x0f;
-    addr[0] = 0x80 | (m_opcode & 0xf);
-    writeValue(addr + 1, 4, delta + 4);
-  }
-  decode(m_ip);
-  assert(isBranch() && m_offSz == 4);
-#endif
+  not_implemented();
 }
-
 uint8_t DecodedInstruction::getModRm() const {
-#if 0 // TODO(gut)
-  assert(m_flags.hasModRm);
-  return m_ip[m_size - m_immSz - m_offSz - m_flags.hasSib - 1];
-#else
+  not_implemented();
   return 0;
-#endif
 }
 
 }
