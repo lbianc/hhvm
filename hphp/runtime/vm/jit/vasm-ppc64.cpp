799cef760609cb2eaa21a683add6b1aeb11b5024
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2016-04-07 10:17:06 -0300

    Revert "Considering link stack optimization on phpret vasm"
    This reverts commit a26ce552fbbe5f3685498b02bcf482ed74b2008a.
    
    That commit caused a regression on the
    hphp/test/quick/debugger/flow_gen_async.php test.

--- hphp/runtime/vm/jit/vasm-ppc64.cpp
+++ hphp/runtime/vm/jit/vasm-ppc64.cpp
@@ -1019,15 +1019,12 @@ void lowerForPPC64(Vout& v, phplogue& inst) {
 }
 
 void lowerForPPC64(Vout& v, phpret& inst) {
-  Vreg return_info = v.makeReg();
-  v << load{inst.fp[AROFF(m_savedRip)], return_info};
+  Vreg tmp = v.makeReg();
+  v << load{inst.fp[AROFF(m_savedRip)], tmp};
   if (!inst.noframe) {
     v << load{inst.fp[AROFF(m_sfp)], inst.d};
   }
-
-  // for balancing the link stack (branch predictor), this should perform blr
-  v << mtlr{return_info};
-  v << ret{RegSet()};
+  v << jmpr{tmp};
 }
 
 /*
