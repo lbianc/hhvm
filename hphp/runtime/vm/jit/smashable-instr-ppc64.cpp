d0de67dbfef2cf13c7a138311cad75e368cbfb0a
Igor Nunes <igor.nunes@eldorado.org.br>
2016-07-06 19:12:44 -0300

    Fix r2 saving on smashable call
    It is not necessary to store r2 on an internal call like the one
    on smashable call. If it is saved, r2 can't be correctly recovered
    after TCExit.
    
    All quick tests are now passing on release and debug mode.
    
    Conflicts:
    	hphp/runtime/vm/jit/smashable-instr-ppc64.cpp

--- hphp/runtime/vm/jit/smashable-instr-ppc64.cpp
+++ hphp/runtime/vm/jit/smashable-instr-ppc64.cpp
@@ -64,14 +64,7 @@ TCA emitSmashableCmpq(CodeBlock& cb, CGMeta& fixups, int32_t imm,
 }
 
 TCA emitSmashableCall(CodeBlock& cb, CGMeta& fixups, TCA target) {
-  auto const start = cb.frontier();
-  meta.smashableLocations.insert(start);
-  Assembler a { cb };
-
-  // verificar se pode ser colocado dentro da macro.
-  a.std(ppc64_asm::reg::r2, ppc64_asm::reg::r1[24]);
-  a.call (target, Assembler::CallArg::SmashInt);
-  return start;
+  return EMIT_BODY(cb, call, Call, target, Assembler::CallArg::SmashInt);
 }
 
 TCA emitSmashableJmp(CodeBlock& cb, CGMeta& fixups, TCA target) {
