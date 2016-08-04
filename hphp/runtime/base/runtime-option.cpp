f6e5fec4b3dbcf584d47527a84d7d36cb7ee7fd7
Gustavo Serra Scalet <gustavo.scalet@eldorado.org.br>
2016-08-04 16:48:42 -0300

    RelocationSize should be the same for all platforms
    It can't be either too big nor too small otherwise it'll simply not
    relocate. Let us stick with 1mb and fix issues we find.
    
    Only if the following error raises then it'll be allowed to set it to a
    bigger size:
    
    message: Attempted to emit 4 byte(s) into a 64 byte DataBlock with 0
    bytes available. This almost certainly means the TC is full. If this is
    the case, increasing Eval.JitASize, Eval.JitAColdSize,
    Eval.JitAFrozenSize and Eval.JitGlobalDataSize in the configuration file
    when running this script or application should fix this problem.

--- hphp/runtime/base/runtime-option.cpp
+++ hphp/runtime/base/runtime-option.cpp
@@ -497,13 +497,7 @@ const uint64_t kEvalVMStackElmsDefault =
  ;
 const uint32_t kEvalVMInitialGlobalTableSizeDefault = 512;
 static const int kDefaultProfileInterpRequests = debug ? 1 : 11;
-static const uint64_t kJitRelocationSizeDefault =
-#if defined(__powerpc64__)
-  16 << 20
-#else
-  1 << 20
-#endif
- ;
+static const uint64_t kJitRelocationSizeDefault = 1 << 20;
 
 static const bool kJitTimerDefault =
 #ifdef ENABLE_JIT_TIMER_DEFAULT
